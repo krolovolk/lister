30196e3eb5a1a6a74f513c8bbdd5209174661e3b
Den <denis.sergei4@yandex.ru>
2022-09-19 12:57:47 +0300

    Add PrimeReact

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import { List } from './components/List/List';
+import { Button } from 'primereact/button';
+
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <List />
+      <Button label="Click" icon="pi pi-check" iconPos="right" />
+    </>
   );
 }
 
---
+++ src/components/List/List.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+export const List: FC<{}> = ({}) => {
+  const n = 5;
+
+  return (
+  <div>
+      <div>Item </div>
+      <div>Item </div>
+      <div>Item </div>
+      <div>Item </div>
+      <div>Item </div>
+  </div>
+ );
+}
